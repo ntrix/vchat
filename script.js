fe793db44753da8daf0f4462923007fc180db1b8
ntrix <ntrix@ntien.com>
2020-10-30 01:56:14 +0100

    fix roomUrl https

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const vm = new Vue({
   },
   computed: {
     roomUrl: function() {
-      return `https://${location.href}?room=${this.roomId}`;
+      return `${location.href}?room=${this.roomId}`;
     }
   },
   async mounted() {
